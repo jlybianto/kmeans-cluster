be9c333e7f6367ae39a467b65b46bb424bf5a9ec
jlybianto <jesse.lybianto@gmail.com>
2016-03-12 20:20:16 -0700

    Import packages and dataset

---
+++ kmeans.py
@@ -0,0 +1,15 @@
+# ----------------
+# IMPORT PACKAGES
+# ----------------
+
+import pandas as pd
+
+# ----------------
+# OBTAIN DATA
+# ----------------
+
+un = pd.read_csv("/un.csv")
+
+# ----------------
+# PROFILE DATA
+# ----------------
