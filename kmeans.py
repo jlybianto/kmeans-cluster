bc928534a1a0a698078dc666bc77514f86633bf5
jlybianto <jesse.lybianto@gmail.com>
2016-03-13 03:28:11 -0600

    Fix data source and start profiling data

--- kmeans.py
+++ kmeans.py
@@ -2,14 +2,20 @@
 # IMPORT PACKAGES
 # ----------------
 
+# The pandas package is used to fetch and store data in a DataFrame.
 import pandas as pd
 
 # ----------------
 # OBTAIN DATA
 # ----------------
 
-un = pd.read_csv("/un.csv")
+# Data imported from a Comma Separated Value document.
+un = pd.read_csv("un.csv")
 
 # ----------------
 # PROFILE DATA
 # ----------------
+
+# Determine the number of observations (rows) and number of features (columns) in the dataset.
+observations = len(un)
+features = un.count()
