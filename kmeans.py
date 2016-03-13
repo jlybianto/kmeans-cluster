71f9df476cd2041ee7b47d9d4abff078f5e08489
jlybianto <jesse.lybianto@gmail.com>
2016-03-13 17:13:21 -0600

    Filter required data

--- kmeans.py
+++ kmeans.py
@@ -17,5 +17,19 @@ un = pd.read_csv("un.csv")
 # ----------------
 
 # Determine the number of observations (rows) and number of features (columns) in the dataset.
-observations = len(un)
+num_observations = len(un)
+num_features = len(un.columns)
 features = un.count()
+print("Number of Data Points (Rows): " + str(num_observations))
+print("")
+print("Number of Features (Columns): " + str(num_features))
+print("")
+print("Number of Data Points per Feature: ")
+print(features)
+print("")
+print("Data Type of each Feature: ")
+print(un.dtypes)
+print("")
+
+# Filter data to columns 6, 7, 8, 9; lifeMale, lifeFemale, infantMortality and GDPperCapita.
+data = un.ix[0:, :10].dropna()
