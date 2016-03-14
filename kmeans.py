6036b37593b3af4a4e13a8ffb97e9feeba69291c
jlybianto <jesse.lybianto@gmail.com>
2016-03-13 18:38:09 -0600

    Model data by determining the centers

--- kmeans.py
+++ kmeans.py
@@ -3,7 +3,10 @@
 # ----------------
 
 # The pandas package is used to fetch and store data in a DataFrame.
+# Whitening to rescale or normalize features through dividing by its standard deviation.
 import pandas as pd
+from scipy.cluster.vq import whiten, kmeans
+from scipy.spatial.distance import cdist
 
 # ----------------
 # OBTAIN DATA
@@ -33,3 +36,23 @@ print("")
 
 # Filter data to columns 6, 7, 8, 9; lifeMale, lifeFemale, infantMortality and GDPperCapita.
 data = un.ix[0:, :10].dropna()
+new_list = []
+for i, j in data.iterrows():
+	new_list.append([j["infantMortality"], j["GDPperCapita"]])
+
+# ----------------
+# MODEL DATA
+# ----------------
+
+# Before running k-means, it is beneficial to rescale each feature dimension of the observation set with whitening.
+# Each feature is divided by its standard deviation across all observations to give it unit variance.
+whiten_list = whiten(new_list)
+
+# Determine the coordinates of centers for a range of k-number clusters.
+k_list = range(1, 11)
+centers = [kmeans(whiten_list, k) for k in k_list]
+# Result: ("A k x N array of k-centroids, Distortion")
+# Distortion is defined as the sume of the squared differences between the observations and the corresponding centroid.
+
+# Append the center coordinates to a new centroid list without the distortion.
+centroids = [center for (center, distortion) in centers]
